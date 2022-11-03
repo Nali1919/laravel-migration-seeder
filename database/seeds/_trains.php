ec258850ad6f373e266ee3762c0693c2b0f30979
Nali1919 <my@email.com>
2022-11-03 17:57:40 +0100

    Concludo esercizio

--- database/seeds/_trains.php
+++ database/seeds/_trains.php
@@ -11,12 +11,22 @@ class _trains extends Seeder
      *
      * @return void
      */
-    public function run(Faker  $Faker)
+    public function run(Faker  $faker)
     {
         for($i=0; $i>100; $i++){
 
             $newtrain = new _train();
-
+            $newtrain->Azienda = $faker->words(1);
+            $newtrain->Stazione_di_partenza = $faker->country();
+            $newtrain->Stazione_di_arrivo = $faker->country();
+            $newtrain->Orario_di_partenza = $faker->time('H:i:s');
+            $newtrain->Orario_di_arrivo = $faker->time('H:i:s');
+            $newtrain->codice_treno = $faker->randomNumber(1,false);
+            $newtrain->numero_carrozze = $faker->randomNumber(1, false);
+            $newtrain->Data_partenza = $faker->date('Y_m_d');
+            $newtrain->In_orario = $faker->boolean();
+            $newtrain->Cancellato = $faker->boolean();
+            $newtrain->save();
 
         }
     }
